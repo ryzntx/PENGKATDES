929c4f31af78930707e74092fa69073d6b20cbf0
ryzntx <thalalatha13@gmail.com>
2024-03-05 21:35:10 +0700

    Add PengaduanModels class

---
+++ app/Models/PengaduanModels.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+class PengaduanModels extends BaseModel
+{
+    protected $table            = 'pengaduans'; // Nama Tabel
+    protected $primaryKey       = 'id_pengaduans'; // Primary Key pada Tabel
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = []; // Field yang boleh diisi
+
+    protected bool $allowEmptyInserts = false;
+}
