a55230bcff73a5cfd0f63b8e46e06d18f3f97bf9
ryzntx <thalalatha13@gmail.com>
2024-03-20 15:35:19 +0700

    Add CSS styles for sidebar toggle button

--- app/Views/layouts/base_layout.php
+++ app/Views/layouts/base_layout.php
@@ -1,53 +1,58 @@
 <!DOCTYPE html>
 <html data-bs-theme="light" lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title><?= $this->renderSection('title'); ?> &mdash; PENGKATDES</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title><?= $this->renderSection('title'); ?> &mdash; PENGKATDES</title>
 
-        <!-- Styles/CSS -->
-        <?= $this->include('layouts/includes/styles') ?>
-    </head>
+    <!-- Styles/CSS -->
+    <?= $this->include('layouts/includes/styles') ?>
+    <style>
+        * {
+            /* border: 1px red solid; */
+        }
+    </style>
+</head>
 
-    <body id="page-top">
-        <div id="wrapper">
-            <!-- Sidebar/Sidemenu -->
-            <?= $this->include('layouts/includes/sidebar') ?>
-            <div class="bg-body d-flex flex-column" id="content-wrapper">
-                <!-- Navbar -->
-                <?= $this->include('layouts/includes/navbar') ?>
-                <div id="content">
-                    <!-- Main Konten -->
-                    <?= $this->renderSection('content'); ?>
+<body id="page-top">
+    <div id="wrapper">
+        <!-- Sidebar/Sidemenu -->
+        <?= $this->include('layouts/includes/sidebar') ?>
+        <div class="bg-body d-flex flex-column" id="content-wrapper">
+            <!-- Navbar -->
+            <?= $this->include('layouts/includes/navbar') ?>
+            <div id="content">
+                <!-- Main Konten -->
+                <?= $this->renderSection('content'); ?>
+            </div>
+            <!-- Footer -->
+            <?= $this->include('layouts/includes/footer'); ?>
+        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="logoutModalLabel">Siap untuk keluar?</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <!-- Footer -->
-                <?= $this->include('layouts/includes/footer'); ?>
-            </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
-        </div>
-
-        <!-- Modal -->
-        <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="logoutModalLabel">Siap untuk keluar?</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Pilih "Keluar" di bawah ini jika Anda siap untuk mengakhiri sesi Anda saat ini.</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                        <a href="<?= base_url('auth/logout') ?>" class="btn btn-danger">Keluar</a>
-                    </div>
+                <div class="modal-body">
+                    <p>Pilih "Keluar" di bawah ini jika Anda siap untuk mengakhiri sesi Anda saat ini.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <a href="<?= base_url('auth/logout') ?>" class="btn btn-danger">Keluar</a>
                 </div>
             </div>
         </div>
+    </div>
 
 
-        <!-- Scripts/JS -->
-        <?= $this->include('layouts/includes/scripts') ?>
-    </body>
+    <!-- Scripts/JS -->
+    <?= $this->include('layouts/includes/scripts') ?>
+</body>
 
 </html>
--- app/Views/layouts/includes/navbar.php
+++ app/Views/layouts/includes/navbar.php
@@ -1,61 +1,51 @@
-<nav
-    class="navbar navbar-expand bg-body-tertiary shadow navbar-light topbar-divider static-top navbar-light rounded-4 mb-3 m-md-3">
-    <div class="container-fluid"><button class="btn rounded-circle me-3" id="sidebarToggleTop" type="button"><i
-                class="fas fa-bars"></i></button>
+<nav class="navbar navbar-expand bg-body-tertiary shadow navbar-light topbar-divider static-top navbar-light rounded-4 mb-3 m-md-3">
+    <div class="container-fluid"><button class="btn rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
         <ul class="navbar-nav align-items-center align-content-center flex-nowrap ms-auto">
-            <li class="theme-switcher nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false"
-                    data-bs-toggle="dropdown" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-                        fill="currentColor" viewBox="0 0 16 16" class="bi bi-sun-fill mb-1">
-                        <path
-                            d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z">
+            <li class="theme-switcher nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-sun-fill mb-1">
+                        <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z">
                         </path>
                     </svg></a>
-                <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item d-flex align-items-center" href="#"
-                        data-bs-theme-value="light"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-                            fill="currentColor" viewBox="0 0 16 16" class="bi bi-sun-fill opacity-50 me-2">
-                            <path
-                                d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z">
+                <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item d-flex align-items-center" href="#" data-bs-theme-value="light"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-sun-fill opacity-50 me-2">
+                            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z">
                             </path>
-                        </svg>Light</a><a class="dropdown-item d-flex align-items-center" href="#"
-                        data-bs-theme-value="dark"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-                            fill="currentColor" viewBox="0 0 16 16" class="bi bi-moon-stars-fill opacity-50 me-2">
-                            <path
-                                d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278">
+                        </svg>Light</a><a class="dropdown-item d-flex align-items-center" href="#" data-bs-theme-value="dark"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-moon-stars-fill opacity-50 me-2">
+                            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278">
                             </path>
-                            <path
-                                d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z">
+                            <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z">
                             </path>
-                        </svg>Dark</a><a class="dropdown-item d-flex align-items-center" href="#"
-                        data-bs-theme-value="auto"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-                            fill="currentColor" viewBox="0 0 16 16" class="bi bi-circle-half opacity-50 me-2">
+                        </svg>Dark</a><a class="dropdown-item d-flex align-items-center" href="#" data-bs-theme-value="auto"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-circle-half opacity-50 me-2">
                             <path d="M8 15A7 7 0 1 0 8 1zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16"></path>
                         </svg>Auto</a></div>
             </li>
             <div class="d-none d-sm-block topbar-divider"></div>
             <li class="nav-item dropdown no-arrow">
                 <div class="nav-item dropdown no-arrow">
-                    <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><img
-                            class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"><span
-                            class="text-body-emphasis d-none d-lg-inline ms-2 small"><?= session('nama') ?></span></a>
-                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
-                        <h6 class="dropdown-header d-flex align-items-center">
-                            <!-- <img class="dropdown-user-img" src="assets/img/illustrations/profiles/profile-1.png"> -->
+                    <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">
+                        <?php
+                        $avatar = new LasseRafn\InitialAvatarGenerator\InitialAvatar();
+                        $user = model(App\Models\UserModels::class)->ambilDataLogin();
+                        echo $avatar->name($user->nama)->rounded()->smooth()->size(40)->autoColor()->generateSvg()->toXMLString();
+                        ?>
+                        <!-- <img
+                            class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"> -->
+                        <!-- <span class="text-body-emphasis d-none d-lg-inline ms-2 small"><?= session('nama') ?></span> -->
+                    </a>
+                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in w-100">
+                        <span class="dropdown-item-text d-flex align-items-center">
+                            <!-- <?php
+                                    $avatar = new LasseRafn\InitialAvatarGenerator\InitialAvatar();
+                                    echo $avatar->name($user->nama)->rounded()->smooth()->size(40)->autoColor()->generateSvg()->toXMLString();
+                                    ?> -->
                             <div class="dropdown-user-details">
-                                <div class="dropdown-user-details-name"><?= session('nama') ?></div>
-                                <div class="dropdown-user-details-email"><?= session('username') ?></div>
+                                <div class="dropdown-user-details-name"><?= $user->nama ?></div>
+                                <div class="dropdown-user-details-email"><?= $user->username ?></div>
                             </div>
-                        </h6>
+                        </span>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#"><i
-                                class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a><a
-                            class="dropdown-item" href="#"><i
-                                class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a>
-                        <a class="dropdown-item" href="#"><i
-                                class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity
+                        <a class="dropdown-item" href="<?= base_url('profilesettings') ?>"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a>
+                        <a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity
                             log</a>
-                        <div class="dropdown-divider"></div><a class="dropdown-item" href="#" data-bs-toggle="modal"
-                            data-bs-target="#logoutModal"><i
-                                class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Keluar</a>
+                        <div class="dropdown-divider"></div><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#logoutModal"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Keluar</a>
                     </div>
                 </div>
             </li>
