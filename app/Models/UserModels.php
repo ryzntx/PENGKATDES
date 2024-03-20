d07b2de59525eb8e8e1d0030c86ac8ca6e406b2b
ryzntx <thalalatha13@gmail.com>
2024-03-20 15:34:28 +0700

    Update UserModels.php to return objects and add ambilDataLogin() method

--- app/Models/UserModels.php
+++ app/Models/UserModels.php
@@ -7,10 +7,15 @@ class UserModels extends BaseModel
     protected $table            = 'users'; // Nama Tabel
     protected $primaryKey       = 'id_users'; // Primary Key pada Tabel
     protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
+    protected $returnType       = 'object';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
     protected $allowedFields    = ['nik', 'nama', 'username', 'password', 'telp', 'level',]; // Field yang boleh diisi
 
     protected bool $allowEmptyInserts = false;
+
+    public function ambilDataLogin()
+    {
+        return $this->where('id_users', session()->get('id_users'))->first();
+    }
 }
